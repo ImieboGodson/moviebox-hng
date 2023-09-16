ab9e81a50b26dc2d9ec3d7e78b76f5495dcf54f2
Godson Imiebo <imiebo.godson@gmail.com>
2023-09-16 20:16:54 +0100

    feat: added loader UI

--- app/movies/search/page.tsx
+++ app/movies/search/page.tsx
@@ -1,5 +1,6 @@
 export const dynamic = "force-dynamic";
 
+import { Suspense } from "react";
 import ClientOnly from "@/app/components/ClientOnly";
 import Container from "@/app/components/Container";
 import EmptyState from "@/app/components/EmptyState";
@@ -7,6 +8,7 @@ import getSearchResults, {
   ISearchParams,
 } from "@/app/actions/getSearchResults";
 import MovieCard from "@/app/components/cards/MovieCard";
+import Loading from "@/app/loading";
 
 interface SearchPageProps {
   searchParams: ISearchParams;
@@ -29,11 +31,13 @@ const SearchPage = async ({ searchParams }: SearchPageProps) => {
     <ClientOnly>
       <Container>
         <div className="w-full py-24">
-          <div className="w-full grid gap-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
-            {results.map((movie: any) => {
-              return <MovieCard key={movie.id} data={movie} />;
-            })}
-          </div>
+          <Suspense fallback={<Loading />}>
+            <div className="w-full grid gap-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
+              {results.map((movie: any) => {
+                return <MovieCard key={movie.id} data={movie} />;
+              })}
+            </div>
+          </Suspense>
         </div>
       </Container>
     </ClientOnly>
