2334bdb4dd7ea878af5031ffdb02ea5cbad69040
Godson Imiebo <imiebo.godson@gmail.com>
2023-09-16 19:17:21 +0100

    fix deploy error

--- app/actions/getSearchResults.ts
+++ app/actions/getSearchResults.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export interface ISearchParams {
-    title: string;
+    title?: string;
 }
 
 export default async function getSearchResults(params:ISearchParams) {
@@ -15,7 +15,9 @@ export default async function getSearchResults(params:ISearchParams) {
 
         const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${title}&include_adult=false&language=en-US&page=1`)
 
-        return response.data.results;
+        const searchResults = await response.data.results;
+
+        return searchResults;
 
     } catch(error: any) {
         throw new Error(error);
