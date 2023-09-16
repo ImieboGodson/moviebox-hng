164b9cacf19b183ed24fc718641f4d12b248584c
Godson Imiebo <imiebo.godson@gmail.com>
2023-09-16 19:30:46 +0100

    fix

--- app/actions/getSearchResults.ts
+++ app/actions/getSearchResults.ts
@@ -1,5 +1,3 @@
-export const dynamic = 'force-dynamic';
-
 import axios from "axios";
 
 export interface ISearchParams {
@@ -10,10 +8,6 @@ export default async function getSearchResults(params:ISearchParams) {
     try {
 
         const { title } = params;
-
-        if(!title) {
-            return;
-        }
 
         const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${title}&include_adult=false&language=en-US&page=1`)
 
